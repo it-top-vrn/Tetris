dcd786534f8c5761b8f79d0820ec3c7c0eb08059
Valentin <ucobenna@mail.ru>
2019-02-21 21:44:40 +0300

    v0.1
    Добавление функций
    void InitFig_I(int position);
    void StartFig(int type, int position);
    bool RowFull(int row);
    int GameScore();
    void DeleteRow();
    bool GameOver();

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,21 +1,182 @@
-﻿// Tetris.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿/*
+	Игра "Тетрис"
+	Учебный проект консольной версии игры "Тетрис"
+	(с) Bardin Valentin, Starinin Andrey, Kitavina Natal'ya, Sanzharovskij Dmitry, Karygin Roman, Frolov Dmitry, Bryantsev Vsevolod, Ivutin Anton, Protsenko Vyacheslav,
+	Protsenko Mikahil.
+	(c) Компьютерная академия ШАГ Воронеж
+	
+	Версия: 0.1 (Февраль 2019 г.)
+*/
 
-#include "pch.h"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+using namespace std;
+
+const int ROW = 20;				// Кол-во строк
+const int COL = 10;				// Кол-во столбцов
+
+int game_place[ROW][COL]{};		// Объявляем массив размером 20x10 (игровое поле) и инициализировали его 0
+
+/*
+	Типы фигур
+	1 - I (Палка)
+	2 - J
+	3 - L
+	4 - O (Квадрат)
+	5 - S
+	6 - Z
+	7 - T
+*/
+
+
+int score = 0;					// Подсчет очков
+int temp;						// Переменная, для подсчета заполненности строки
+
+void InitFig_I(int position);
+void InitFig_J(int position);
+void InitFig_L(int position);
+void InitFig_O();
+void InitFig_S(int position);
+void InitFig_Z(int position);
+void InitFig_T(int position);
+void StartFig(int type, int position); // Описываем фигуры
+bool RowFull(int row);			// Определение заполненности строки
+int GameScore();				// Определение очков
+void DeleteRow();				// Удаление заполенной строки
+bool GameOver();				// Определение проигрыша
+
+
+int main() {
+	
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void InitFig_I(int position) {
+	switch (position) {
+		case 1: { // Горизонтальное положение
+			game_place[0][3] == 1;
+			game_place[0][4] == 1;
+			game_place[0][5] == 1;
+			game_place[0][6] == 1;
+			break;
+		}
+		case 2: { // Вертикальное положение
+			game_place[0][5] == 1;
+			game_place[1][5] == 1;
+			game_place[2][5] == 1;
+			game_place[3][5] == 1;
+			break;
+		}
+	}
+}
+void InitFig_J(int position) {
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+}
+void InitFig_L(int position) {
+
+}
+void InitFig_O() {
+
+}
+void InitFig_S(int position) {
+
+}
+void InitFig_Z(int position) {
+
+}
+void InitFig_T(int position) {
+
+}
+
+void StartFig(int type, int position) {
+	switch (type) {
+		case 1: InitFig_I(position);
+			break;
+		case 2: InitFig_J(position);
+			break;
+		case 3: InitFig_L(position);
+			break;
+		case 4: InitFig_O();
+			break;
+		case 5: InitFig_S(position);
+			break;
+		case 6: InitFig_Z(position);
+			break;
+		case 7: InitFig_T(position);
+			break;
+		default:
+			break;
+	}
+}
+
+bool RowFull(int row) {
+	temp = 0;
+	for (int j = 0; j < COL; j++) {
+		temp += game_place[row][j];
+	}
+
+	if (temp == 10) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int GameScore() {
+	int k = 0;					// Переменная для подсчета заполненных строк
+	for (int i = 0; i < ROW; i++) {	
+		if (RowFull(i)) {		// Если строка заполенная, то увеличиваем счетчик заполненых строк на 1
+			k++;
+		}
+	}
+								// Добавление очков при полном заполнении строки 
+	switch (k) {
+		case 1: {
+			return 100;			// При заполнении 1 строки 100 очков
+			break;
+		}
+		case 2: {
+			return 300;			// При заполнении 2 строк 300 очков
+			break;
+		}
+		case 3: {
+			return 700;			// При заполнении 3 строк 700 очков
+			break;
+		}
+		case 4: {
+			return 1500;		// При заполнении 4 строк 1500 очков
+			break;
+		}
+		default: {
+			return 0;
+			break;
+		}
+	}
+}
+
+void DeleteRow() {
+	int m = 0;
+
+	for (int i = 0; i < ROW; i++) {
+		if (RowFull(i)) {		// Если строка заполненная, то заменяем ее строкой выше
+			m = i;
+			do {
+				for (int j = 0; j > COL; j++) {
+					game_place[m][j] == game_place[m - 1][j];
+				}
+				m--;
+			} while (m != 0);
+		}
+	}
+}
+
+bool GameOver() {				// Функция для проверки верхней строчки игрового поля на наличие в ней 1 (признак фигуры), если есть хоть одна то игра окончена	
+	for (int i = 0; i < COL; i++) {
+		if (game_place[0][i] == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
