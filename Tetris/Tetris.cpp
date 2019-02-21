16ff7407b8dd6862e41ed61641cd279beb519fc7
RichiArt <7776171@mail.ru>
2019-02-21 21:44:27 +0300

    Начало разработки
    Добавили функции:
    определения проигрыша
    определения очков
    удаления строки при ее заполнении
    появления фигуры I

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,21 +1,163 @@
-﻿// Tetris.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿/*
+Игра "Тетрис"
+Учебный проект консольной версии игры "Тетрис"
+(c) Карыгин Роман, Санжаровский Дмитрий, Фролов Дмитрий, Китавина Наталья, Бардин Валентин,
+Брянцев Всеволод, Проценко Михаил, Проценко Вячеслов, Ивутин Антон, Старинин Андрей.
+(с) Компьютерная академия ШАГ. Воронеж
+Версия 0.1 (21.02.2019)
+*/
 
 #include "pch.h"
 #include <iostream>
+using namespace std;
+
+const int ROW = 20; // строки
+const int COL = 10; // столбцы
+int game_place[ROW][COL] = {}; // объявляем двумерный массив игрового поля ( {} - заполнение массива 0)
+
+/*
+	Типы фигур:
+	1 - I (палка)
+	2 - J 
+	3 - L
+	4 - O (квадрат)
+	5 - S
+	6 - Z
+	7 - T
+*/
+
+int score = 0; // переменная, подсчитывающая очки в игре
+int temp, // переменная темп - для подсчета суммы строк,
+
+void InitFig_I(int position); //Функция появления фигуры I
+void InitFig_J(int position);
+void InitFig_L(int position);
+void InitFig_O();
+void InitFig_S(int position);
+void InitFig_Z(int position);
+void InitFig_T(int position);
+void StartFigures(int type, int position); // Функция старта фигур
+bool RowFull(int row);
+bool GameOver(); // функция определения проигрыша
+int GameScore(); // Функция определения очков
+void DeleteRow(); // Функция удаления строки при ее заполнении
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	setlocale(LC_ALL, "rus");
+}
+void InitFig_I(int position)
+{
+	switch (position)
+	{
+	case 1: // горизонтальное положение
+	{
+		game_place[0][3] = 1;
+		game_place[0][4] = 1;
+		game_place[0][5] = 1;
+		game_place[0][6] = 1;
+
+	}
+		break;
+	case 2: // вертикальное положение
+	{
+		game_place[0][5] = 1;
+		game_place[1][5] = 1;
+		game_place[2][5] = 1;
+		game_place[3][5] = 1;
+	}
+		break;
+	}
+}
+void StartFigures(int type, int position) 
+{
+	switch (type)
+	{
+	case 1: InitFig_I(position);
+		break;
+	case 2: InitFig_J(position);
+		break;
+	case 3: InitFig_L(position);
+		break;
+	case 4: InitFig_O();
+		break;
+	case 5: InitFig_S(position);
+		break;
+	case 6: InitFig_Z(position);
+		break;
+	case 7: InitFig_T(position);
+		break;
+	default:
+		break;
+	}
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+bool GameOver()
+{	// функция проверяет верхнюю строчку на наличие в ней "1". Если есть хоть одна "1", игра окончена.
+	for (int i = 0; i < COL; i++)
+	{
+		if (game_place[0][i] == 1)
+			return true;
+		else
+			return false;
+	}
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+bool RowFull(int row)
+{
+	int temp = 0;
+	for (int j = 0; j < COL; j++)
+	{
+		temp = temp + game_place[row][j];
+	}
+	if (temp == 10)
+		return true;
+	else
+		return false;
+}
+
+int GameScore()
+{
+	
+	int k=0;  //переменная к - подсчитывает количество строк, сумма в которых равна 10
+	for (int i = 0; i < ROW; i++)
+	{
+		
+		if (RowFull(i)) // если строка заполненна, то увеличиваем счетчик
+		{
+			k++;
+		}
+	}
+	switch (k) // если одновременно заполненных строк 1 - 100 очков, 2 - 300 очков, 3 - 700 очков, 4 - 1500 очков, 0 - возврат.
+	{
+	case 1: return 100; break;
+	case 2: return 300; break;
+	case 3: return 700; break;
+	case 4: return 1500; break;
+	default: return 0; break;
+	}
+}
+
+void DeleteRow()
+{
+	int m = 0; // временная переменная для внесения адреса заполненной строки
+	for (int i = 0; i < ROW; i++)
+	{
+		if (RowFull(i))
+		{
+			m = i; // находим, какая строка заполнена
+			do 
+			{
+				for (int j = 0; j < COL; j++)
+				{
+					game_place[m][j] = game_place[m - 1][j];
+				}
+				m--;
+			}while (m!=0)
+		}
+	}
+}
+
